9a5118fc120fad2b664e205e76e9d46fd86d8ab0
nisha-codespace <nishanestle3108@gmail.com>
2026-08-18 19:29:32 +0530

    Add temporary email generation and copy functionality

--- temp-email/src/App.jsx
+++ temp-email/src/App.jsx
@@ -1,6 +1,51 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App(){
+
+  const [email, setEmail] = useState("");
+  const [Copied, setCopied] = useState(false);
+  const [inbox, setInbox] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(600);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((previousTIme) => {
+        
+        if(previousTime <= 1){
+          clearInterval(timer);
+          return 0;
+        }
+
+        return previousTime - 1;
+      });
+    },1000);
+
+    return () => clearInterval(timer);
+
+  },[]);
+
+  // generate Random EMail
+  const generateEmail = () => {
+    const randomNumber = Math.floor(Math.random()*100000);
+    const newEmail =`user${randomNumber}@tempemail.com`;
+    setEmail(newEmail);
+    setCopied(false);
+  };
+
+  //Copy email
+  const copyEmail = () => {
+    navigator.clipboard.writeText(email);
+    setCopied(true);
+
+    setTimeout(() =>{
+      setCopied(false);
+    },2000);
+  };
+   
+  const minutes = Math.floor(timeLeft / 60);
+
+  const seconds = timeLeft % 60;
   return (
     <div className="app">
 
@@ -20,16 +65,22 @@ function App(){
           <p>Get a temporary email address to protect your privacy and avoid spam.</p>
 
           <div className="email-box">
-            <span>email@tempemail.com</span>
-            <button>Copy</button>
+            <span>{email || "click generate new email"}</span>
+            <button 
+              onClick={copyEmail}
+              disabled = {!email}>
+              {Copied ? "Copied!" : "Copy"}
+            </button>
           </div>
 
-          <button className="generate-btn">
+          <button className="generate-btn" onClick={generateEmail}>
             Generate New Email
           </button>
 
           <p className="expiry">
-            This email address will expire in 10 minutes. 
+            This email address will expire in{" "}
+            {String(minutes).padStart(2,"0")}:
+            {String(seconds).padStart(2,"0")}
           </p>
         </section>
 
